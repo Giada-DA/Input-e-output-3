a959b1c416cda1e19f5d1cbb7a8cd3db4aff27aa
Giada Dall'Ara <giada.25700@gmail.com>
2023-01-29 16:06:06 +0100

    Esercizio 3 sul print

---
+++ Esercizio/Start.java
@@ -0,0 +1,10 @@
+public class PrintYourNameAndSurname03 {
+    public static void main(String[] args) {
+        String name = "Anastasia";
+        String surname = "Romanov";
+        System.out.println("Your name is: " + name);
+        System.out.println("Your surname is: " + surname);
+        //alternativa
+        // System.out.printf("Your name is %s %nYour surname is %s", name,surname);
+    }
+}
